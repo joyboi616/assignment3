6cf72a937e36df432fd9c5db1a0bfdc5af79abd6
joyboi616 <200455316@student.georgianc.on.ca>
2022-08-02 20:56:27 -0400

    updated files

--- movie-franchise2/src/App.js
+++ movie-franchise2/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 
-import Navbar from './components/navbar';
-import Home from "./components/home";
+import Navbar from './components/Navbar';
+import Home from "./components/Home";
 
-import MarvelMoviesList from './components/marvel/marvelMoviesList';
-import MarvelMoviesCreate from './components/marvel/marvelMoviesCreate';
-import MarvelMoviesEdit from './components/marvel/marvelMoviesEdit';
+import MarvelMoviesList from './components/marvel/MarvelMoviesList';
+import MarvelMoviesCreate from './components/marvel/MarvelMoviesCreate';
+import MarvelMoviesEdit from './components/marvel/MarvelMoviesEdit';
 
 function App() {
-  return(
+  return (
     <>
       <Navbar />
       <div className="container">
--- movie-franchise2/src/components/marvel/marvelMoviesCreate.js
+++ movie-franchise2/src/components/marvel/marvelMoviesCreate.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
-/* import DatePicker from 'react-datepicker'; */
+/* import axios from 'axios'; */
+import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class MarvelMoviesCreate extends Component {
   constructor(props) {
     super(props);
-
-    this.onChangeMovieTitle = this.onChangeMovieTitle.bind(this);
-    this.onChangeDirector = this.onChangeDirector.bind(this);
-    this.onChangeReleaseDate = this.onChangeReleaseDate.bind(this);
-    this.onChangeBoxOffice = this.onChangeBoxOffice.bind(this);
-    this.onChangeRottenTomatoes = this.onChangeRottenTomatoes.bind(this);
-    
-    this.onSubmit = this.onSubmit.bind(this);
 
     this.state = {
       movieTitle: '',
@@ -20,99 +13,15 @@ export default class MarvelMoviesCreate extends Component {
       releaseDate: new Date(),
       boxOffice: '',
       rottenTomatoes: '',
-      marvelMovies: []
+      users: []
     }
-  }
-
-  componentDidMount(e) {
-    this.setState({
-      marvelMovies: ['test movie'],
-      movieTitle: 'test movie',
-      director: 'test movie',
-      releaseDate: 'test movie',
-      boxOffice: 'test movie',
-      rottenTomatoes: 'test movie'
-    })
-  }
-
-  onChangeMovieTitle(e) {
-    this.setState({
-      movieTitle: e.target.value
-    });
-  }
-
-  onChangeDirector(e) {
-    this.setState({
-      director: e.target.value
-    });
-  }
-
-  onChangeReleaseDate(date) {
-    this.setState({
-      releaseDate: date
-    });
-  }
-
-  onChangeBoxOffice(e) {
-    this.setState({
-      boxOffice: e.target.value
-    });
-  }
-
-  onChangeRottenTomatoes(e) {
-    this.setState({
-      rottenTomatoes: e.target.value
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    const marvelMovie = {
-      movieTitle: this.state.movieTitle,
-      director: this.state.director,
-      releaseDate: this.state.releaseDate,
-      boxOffice: this.state.boxOffice,
-      rottenTomatoes: this.state.rottenTomatoes,
-    }
-
-    console.log(marvelMovie)
-
-    window.location = '/marvelList'
   }
 
   render() {
     return (
-    <div>
-      <h3>Create New Marvel Movie</h3>
-      <form onSubmit={this.onSubmit}>
-        <div className="form-group"> 
-          <label>Movie Title: </label>
-          <select ref="userInput"
-            required
-            className="form-control"
-            value={this.state.movieTitle}
-            onChange={this.onChangeMovieTitle}>
-            {
-              this.state.movieTitle.map(function(movieTitle) {
-                return <option 
-                  key={movieTitle}
-                  value={movieTitle}>{movieTitle}
-                  </option>;
-              })
-            }
-          </select>
-
-
-
-        </div>
-        <div className="form-group">
-          <input type="submit" value="Create Marvel Movie" className="btn btn-primary" />
-        </div>
-      </form>
-    </div>
+      <div>
+        <p>create</p>
+      </div>
     )
   }
 }
-
-
